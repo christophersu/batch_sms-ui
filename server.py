90cf82cc267b315ede303a66f02cdcdf30865420
Christopher Su <christophersu9@gmail.com>
2015-10-01 16:25:57 -0700

    form

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from basic_auth import requires_auth
 import batch_sms
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 @requires_auth
 def index():
-    return 'Hello World!'
+    temp = ['this', 'is a', 'subscriber', 'list']
+    return render_template('main.html', subscriber_lists=temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
