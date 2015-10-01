ac531792962e7a2a4f4258f3ffdae564ffd1d398
Christopher Su <christophersu9@gmail.com>
2015-10-01 15:59:54 -0700

    basic auth

--- server.py
+++ server.py
@@ -1,30 +1,9 @@
-from flask import Flask, request, Response
+from flask import Flask
 from functools import wraps
 
+from basic_auth import requires_auth
 import batch_sms
 import secrets
-
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
-    return username == secrets.BASIC_AUTH_USERNAME and password == secrets.BASIC_AUTH_PASSWORD
-
-def authenticate():
-    """Sends a 401 response that enables basic auth"""
-    return Response(
-    'Could not verify your access level for that URL.\n'
-    'You have to login with proper credentials', 401,
-    {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-def requires_auth(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
-        return f(*args, **kwargs)
-    return decorated
 
 app = Flask(__name__)
 
